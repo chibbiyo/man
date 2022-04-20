a21ec081240dbd8bcf2916fb5f5707c755f9fa8f
chibbiyo <103373807+chibbiyo@users.noreply.github.com>
2022-04-20 20:37:22 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,59 @@
+//═══════[© 2022 Rei Bot Inc.]════════\\
+
+//~you can re-upload but tag my channel
+//or put my channel link in the description.
+//~you can recode or copy the codes but give
+//credit.
+//~Thank you to LORD BUDDHA, ME AND MY 
+//FAMILY.
+
+//═══════[modules]════════\\
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//═══════[api website]════════\\
+global.APIs = {
+	zenz: 'https://zenzapi.xyz', //credit zenz, thank you so much bro <3
+}
+//═══════[api key of the website]════════\\
+global.APIKeys = {
+	'https://zenzapi.xyz': '805a6c3fa9', //thank you to zenz
+}
+
+//═══════[modification]════════\\
+global.owner = ['212655892921','212667876859','393509549696'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['212655892921'] //another owner number
+global.premium = ['212655892921'] //premium number
+global.pengguna = 'chibbiyo' //username
+global.botnma = 'Rei-Bot' //bot name
+global.ownernma = 'sam' //owner name
+global.packname = 'ReiBot' //sticker package name
+global.author = 'By sam' //sticker author name
+global.sessionName = 'session' //session name
+global.prefa = ['#',''] //prefix
+global.sp = '⭔' //design
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This feature in only for the bot number',
+    wait: 'In process...',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+}
+global.limitawal = {
+    premium: "Infinity", //premium user limit
+    free: 100 //free user limit
+}
+global.thumb = fs.readFileSync('./XeonMedia/cheemspic.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
